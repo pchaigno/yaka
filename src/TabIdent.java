cffeeaba5affea4efeb55a53df1fb879722097d7
Paul Chaignon <paul.chaignon@insa-rennes.fr>
2013-02-15 09:11:22 +0100

    Table des identificateurs pour les expressions.

--- src/TabIdent.java
+++ src/TabIdent.java
@@ -3,19 +3,38 @@ import java.util.HashMap;
 public class TabIdent {
 	private HashMap<String, Ident> table;
 	
+	/**
+	 * Constructor
+	 * @param taille
+	 */
 	public TabIdent(int taille) {
-		
+		this.table = new HashMap<String, Ident>(taille); 
 	}
 	
+	/**
+	 * Search a key in the map.
+	 * @param clef The key to search.
+	 * @return The value corresponding to the key.
+	 */
 	public Ident chercheIdent(String clef) {
-		return null;
+		return this.table.get(clef);
 	}
 	
+	/**
+	 * Search a key in the map.
+	 * @param clef The key to search.
+	 * @return True if the map contain the key clef.
+	 */
 	public boolean existeIdent(String clef) {
-		return false;
+		return this.table.containsKey(clef);
 	}
 	
+	/**
+	 * Add a couple (key, value).
+	 * @param clef The key.
+	 * @param id The value.
+	 */
 	public void rangeIdent(String clef, Ident id) {
-		
+		this.table.put(clef, id);
 	}
 }
