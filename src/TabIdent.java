310654b339775224b3373514cd5705a45a50458e
Paul Chaignon <paul.chaignon@insa-rennes.fr>
2013-02-15 08:58:57 +0100

    First commit

---
+++ src/TabIdent.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class TabIdent {
+	private HashMap<String, Ident> table;
+	
+	public TabIdent(int taille) {
+		
+	}
+	
+	public Ident chercheIdent(String clef) {
+		return null;
+	}
+	
+	public boolean existeIdent(String clef) {
+		return false;
+	}
+	
+	public void rangeIdent(String clef, Ident id) {
+		
+	}
+}
