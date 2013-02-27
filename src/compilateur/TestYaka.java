a58c81df011d0cb80444821d7cddb9026ad3e370
Paul Chaignon <midionos@laposte.net>
2013-02-27 22:23:30 +0100

    Adding tests for the ASM code generation.

--- src/compilateur/TestYaka.java
+++ src/compilateur/TestYaka.java
@@ -6,9 +6,11 @@ import java.io.InputStream;
 import junit.framework.TestCase;
 
 /**
- * Unit tests
+ * Unit tests.
  */
 public class TestYaka extends TestCase {
+	boolean showYVMCode = false;
+	boolean showASMCode = true;
 	
 	/**
 	 * First test just to intialize the Yaka compiler.
@@ -18,26 +20,6 @@ public class TestYaka extends TestCase {
 	}
 	
 	/**
-	 * Test an empty program.
-	 */
-	/*public void testEmpty() {
-		String program = "PROGRAMME empty FPROGRAMME";
-		this.launchAnalyse(program);
-		assertEquals("", Yaka.yvm.getProgram());
-	}*/
-	
-	/**
-	 * Test the declaration of the constants.
-	 */
-	/*public void testDeclarationConstants() {
-		String program = "PROGRAMME const " +
-				"CONST aa=10, ba=VRAI, cc=aa;" +
-				"FPROGRAMME";
-		this.launchAnalyse(program);
-		assertEquals("", Yaka.yvm.getProgram());
-	}*/
-	
-	/**
 	 * Test the declaration of the variables.
 	 */
 	public void testDeclarationVariables() {
@@ -45,10 +27,46 @@ public class TestYaka extends TestCase {
 				"VAR ENTIER c1,c2; VAR BOOLEEN b1,b2; " +
 				"c1 = 5; " +
 				"FPROGRAMME";
-		String programYVM = "iconst 5\n" +
-				"istore -2\n";
-		this.launchAnalyse(program);
+		
+		String programYVM = "entete\n" +
+				"ouvrePrinc 8\n" +
+				"iconst 5\n" +
+				"istore -2\n" +
+				"queue\n";
+		compileToYVM(program);
+		if(this.showYVMCode) {
+			System.out.println("Variables declaration in YVM:");
+			System.out.println(Yaka.yvm.getProgram());
+		}
 		assertEquals(programYVM, Yaka.yvm.getProgram());
+		
+		String programASM = "; entete\n" +
+				"extrn lirent:proc, ecrent:proc\n" +
+				"extrn ecrbool:proc\n" +
+				"extrn ecrch:proc, ligsuiv:proc\n" +
+				".model SMALL\n" +
+				".586\n\n" +
+				".CODE\n" +
+				"debut :\n" +
+				"STARTUPCODE\n\n" +
+				"; ouvrePrinc 8\n" +
+				"mov bp, sp\n" +
+				"sub sp, 8\n\n" +
+				"; iconst 5\n" +
+				"push word ptr 5\n\n" +
+				"; istore -2\n" +
+				"pop ax\n" +
+				"mov word ptr [bp-2], ax\n\n" +
+				"; queue\n" +
+				"nop\n" +
+				"EXITCODE\n" +
+				"end debut\n\n";
+		compileToASM(program);
+		if(this.showASMCode) {
+			System.out.println("Variables declaration in ASM:");
+			System.out.println(Yaka.yvm.getProgram());
+		}
+		assertEquals(programASM, Yaka.yvm.getProgram());
 	}
 	
 	/**
@@ -60,46 +78,47 @@ public class TestYaka extends TestCase {
 				"VAR ENTIER c1,c2; VAR BOOLEEN b1,b2; " +
 				"c1 = 5; " +
 				"FPROGRAMME";
-		String programYVM = "iconst 5\n" +
-				"istore -2\n";
-		this.launchAnalyse(program);
+		
+		String programYVM = "entete\n" +
+				"ouvrePrinc 8\n" +
+				"iconst 5\n" +
+				"istore -2\n" +
+				"queue\n";
+		compileToYVM(program);
+		if(this.showYVMCode) {
+			System.out.println("Declaration in YVM:");
+			System.out.println(Yaka.yvm.getProgram());
+		}
 		assertEquals(programYVM, Yaka.yvm.getProgram());
+		
+		String programASM = "; entete\n" +
+				"extrn lirent:proc, ecrent:proc\n" +
+				"extrn ecrbool:proc\n" +
+				"extrn ecrch:proc, ligsuiv:proc\n" +
+				".model SMALL\n" +
+				".586\n\n" +
+				".CODE\n" +
+				"debut :\n" +
+				"STARTUPCODE\n\n" +
+				"; ouvrePrinc 8\n" +
+				"mov bp, sp\n" +
+				"sub sp, 8\n\n" +
+				"; iconst 5\n" +
+				"push word ptr 5\n\n" +
+				"; istore -2\n" +
+				"pop ax\n" +
+				"mov word ptr [bp-2], ax\n\n" +
+				"; queue\n" +
+				"nop\n" +
+				"EXITCODE\n" +
+				"end debut\n\n";
+		compileToASM(program);
+		if(this.showASMCode) {
+			System.out.println("Declaration in ASM:");
+			System.out.println(Yaka.yvm.getProgram());
+		}
+		assertEquals(programASM, Yaka.yvm.getProgram());
 	}
-	
-	/**
-	 * Test the program for expressions part.
-	 */
-	/*public void testExpression() {
-		String program = "PROGRAMME declar " +
-				"CONST aa=10, ba=VRAI, cc=aa; " +
-				"VAR ENTIER c1,c2; VAR BOOLEEN b1,b2; " +
-				"(aa+cc/2)/5; c1+3*c1-aa; c1<=(c2+4); ba OU VRAI;" +
-				"FPROGRAMME";
-		String programYVM = "iconst 10\n"+
-				"iconst 10\n"+
-				"iconst 2\n"+
-				"idiv\n"+
-				"iadd\n"+
-				"iconst 5\n"+
-				"idiv\n"+
-				"iload -2\n"+
-				"iconst 3\n"+
-				"iload -2\n"+
-				"imul\n"+
-				"iadd\n"+
-				"iconst 10\n"+
-				"isub\n"+
-				"iload -2\n"+
-				"iload -4\n"+
-				"iconst 4\n"+
-				"iadd\n"+
-				"iconst -1\n"+
-				"iconst -1\n"+
-				"ior\n"+
-				"iinfegal\n";
-		this.launchAnalyse(program);
-		assertEquals(programYVM, Yaka.yvm.getProgram());
-	}*/
 	
 	/**
 	 * Test the program for affectations part.
@@ -109,7 +128,10 @@ public class TestYaka extends TestCase {
 				"VAR ENTIER x,y,z; " +
 				"z=(x+y/2)/5; x=y+3*y-4;" +
 				"FPROGRAMME";
-		String programYVM = "iload -2\n"+
+		
+		String programYVM = "entete\n" +
+				"ouvrePrinc 6\n" +
+				"iload -2\n"+
 				"iload -4\n"+
 				"iconst 2\n"+
 				"idiv\n"+
@@ -124,9 +146,91 @@ public class TestYaka extends TestCase {
 				"iadd\n"+
 				"iconst 4\n"+
 				"isub\n"+
-				"istore -2\n";
-		this.launchAnalyse(program);
+				"istore -2\n" +
+				"queue\n";
+		compileToYVM(program);
+		if(this.showYVMCode) {
+			System.out.println("Affectations in YVM:");
+			System.out.println(Yaka.yvm.getProgram());
+		}
 		assertEquals(programYVM, Yaka.yvm.getProgram());
+		
+		String programASM = "; entete\n" +
+				"extrn lirent:proc, ecrent:proc\n" +
+				"extrn ecrbool:proc\n" +
+				"extrn ecrch:proc, ligsuiv:proc\n" +
+				".model SMALL\n" +
+				".586\n\n" +
+				".CODE\n" +
+				"debut :\n" +
+				"STARTUPCODE\n\n" +
+				"; ouvrePrinc 6\n" +
+				"mov bp, sp\n" +
+				"sub sp, 6\n\n" +
+				"; iload -2\n" +
+				"push word ptr [bp-2]\n\n" +
+				"; iload -4\n" +
+				"push word ptr [bp-4]\n\n" +
+				"; iconst 2\n" +
+				"push word ptr 2\n\n" +
+				"; idiv\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"cwd\n" +
+				"idiv bx\n" +
+				"push ax\n\n" +
+				"; iadd\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"add ax, bx\n" +
+				"push ax\n\n" +
+				"; iconst 5\n" +
+				"push word ptr 5\n\n" +
+				"; idiv\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"cwd\n" +
+				"idiv bx\n" +
+				"push ax\n\n" +
+				"; istore -6\n" +
+				"pop ax\n" +
+				"mov word ptr [bp-6], ax\n\n" +
+				"; iload -4\n" +
+				"push word ptr [bp-4]\n\n" +
+				"; iconst 3\n" +
+				"push word ptr 3\n\n" +
+				"; iload -4\n" +
+				"push word ptr [bp-4]\n\n" +
+				"; imul\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"imul bx\n" +
+				"push ax\n\n" +
+				"; iadd\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"add ax, bx\n" +
+				"push ax\n\n" +
+				"; iconst 4\n" +
+				"push word ptr 4\n\n" +
+				"; isub\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"sub ax, bx\n" +
+				"push ax\n\n" +
+				"; istore -2\n" +
+				"pop ax\n" +
+				"mov word ptr [bp-2], ax\n\n" +
+				"; queue\n" +
+				"nop\n" +
+				"EXITCODE\n" +
+				"end debut\n\n";
+		compileToASM(program);
+		if(this.showASMCode) {
+			System.out.println("Affectations in ASM:");
+			System.out.println(Yaka.yvm.getProgram());
+		}
+		assertEquals(programASM, Yaka.yvm.getProgram());
 	}
 	
 	/**
@@ -147,18 +251,191 @@ public class TestYaka extends TestCase {
 				"z=(x+y/2)/5; " +
 				"x=y+3*y-4; " +
 				"FPROGRAMME";
-		this.launchAnalyse(program);
-		System.out.println(Yaka.yvm.getProgram());
+		
+		String programYVM = "entete\n" +
+				"ouvrePrinc 6\n" +
+				"ecrireChaine \"x=\"\n" +
+				"lireEnt -2\n" +
+				"aLaLigne\n" +
+				"ecrireChaine \"y=\"\n" +
+				"lireEnt -4\n" +
+				"aLaLigne\n" +
+				"ecrireChaine \"x+y=\"\n" +
+				"iload -2\n" +
+				"iload -4\n" +
+				"iadd\necrireEnt\n" +
+				"aLaLigne\n" +
+				"iload -2\n" +
+				"iload -4\n" +
+				"iconst 2\n" +
+				"idiv\n" +
+				"iadd\n" +
+				"iconst 5\n" +
+				"idiv\n" +
+				"istore -6\n" +
+				"iload -4\n" +
+				"iconst 3\n" +
+				"iload -4\n" +
+				"imul\n" +
+				"iadd\n" +
+				"iconst 4\n" +
+				"isub\n" +
+				"istore -2\n" +
+				"queue\n";
+		compileToYVM(program);
+		if(this.showYVMCode) {
+			System.out.println("Inputs and outputs in YVM:");
+			System.out.println(Yaka.yvm.getProgram());
+		}
+		assertEquals(programYVM, Yaka.yvm.getProgram());
+		
+		String programASM = "; entete\n" +
+				"extrn lirent:proc, ecrent:proc\n" +
+				"extrn ecrbool:proc\n" +
+				"extrn ecrch:proc, ligsuiv:proc\n" +
+				".model SMALL\n" +
+				".586\n\n" +
+				".CODE\n" +
+				"debut :\n" +
+				"STARTUPCODE\n\n" +
+				"; ouvrePrinc 6\n" +
+				"mov bp, sp\n" +
+				"sub sp, 6\n\n" +
+				"; ecrireChaine \"x=\"\n" +
+				".DATA\n" +
+				"mess0 DB \"x=$\" \n" +
+				".CODE\n" +
+				"lea dx, mess0\n" +
+				"push dx\n" +
+				"call ecrch\n\n" +
+				"; lireEnt -2\n" +
+				"lea dx, [bp-2]\n" +
+				"push dx\n" +
+				"call lirent\n\n" +
+				"; aLaLigne\n" +
+				"call ligsuiv\n\n" +
+				"; ecrireChaine \"y=\"\n" +
+				".DATA\n" +
+				"mess1 DB \"y=$\" \n" +
+				".CODE\n" +
+				"lea dx, mess1\n" +
+				"push dx\n" +
+				"call ecrch\n\n" +
+				"; lireEnt -4\n" +
+				"lea dx, [bp-4]\n" +
+				"push dx\n" +
+				"call lirent\n\n" +
+				"; aLaLigne\n" +
+				"call ligsuiv\n\n" +
+				"; ecrireChaine \"x+y=\"\n" +
+				".DATA\n" +
+				"mess2 DB \"x+y=$\" \n" +
+				".CODE\n" +
+				"lea dx, mess2\n" +
+				"push dx\n" +
+				"call ecrch\n\n" +
+				"; iload -2\n" +
+				"push word ptr [bp-2]\n\n" +
+				"; iload -4\n" +
+				"push word ptr [bp-4]\n\n" +
+				"; iadd\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"add ax, bx\n" +
+				"push ax\n\n" +
+				"; ecrireEnt\n" +
+				"call ecrent\n\n" +
+				"; aLaLigne\n" +
+				"call ligsuiv\n\n" +
+				"; iload -2\n" +
+				"push word ptr [bp-2]\n\n" +
+				"; iload -4\n" +
+				"push word ptr [bp-4]\n\n" +
+				"; iconst 2\n" +
+				"push word ptr 2\n\n" +
+				"; idiv\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"cwd\n" +
+				"idiv bx\n" +
+				"push ax\n\n" +
+				"; iadd\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"add ax, bx\n" +
+				"push ax\n\n" +
+				"; iconst 5\n" +
+				"push word ptr 5\n\n" +
+				"; idiv\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"cwd\n" +
+				"idiv bx\n" +
+				"push ax\n\n" +
+				"; istore -6\n" +
+				"pop ax\n" +
+				"mov word ptr [bp-6], ax\n\n" +
+				"; iload -4\n" +
+				"push word ptr [bp-4]\n\n" +
+				"; iconst 3\n" +
+				"push word ptr 3\n\n" +
+				"; iload -4\n" +
+				"push word ptr [bp-4]\n\n" +
+				"; imul\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"imul bx\n" +
+				"push ax\n\n" +
+				"; iadd\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"add ax, bx\n" +
+				"push ax\n\n" +
+				"; iconst 4\n" +
+				"push word ptr 4\n\n" +
+				"; isub\n" +
+				"pop bx\n" +
+				"pop ax\n" +
+				"sub ax, bx\n" +
+				"push ax\n\n" +
+				"; istore -2\n" +
+				"pop ax\n" +
+				"mov word ptr [bp-2], ax\n\n" +
+				"; queue\n" +
+				"nop\n" +
+				"EXITCODE\n" +
+				"end debut\n\n";
+		compileToASM(program);
+		if(this.showASMCode) {
+			System.out.println("Inputs and outputs in ASM:");
+			System.out.println(Yaka.yvm.getProgram());
+		}
+		assertEquals(programASM, Yaka.yvm.getProgram());
 	}
 	
 	/**
-	 * Compile the program.
-	 * @param program The program.
+	 * Compile the program to YVM code.
+	 * @param program The Yaka program.
 	 */
-	private void launchAnalyse(String program) {
+	private static void compileToYVM(String program) {
 		InputStream input = new ByteArrayInputStream(program.getBytes());
 		Yaka.ReInit(input);
-		Yaka.initVariables();
+		Yaka.initVariables(true);
+	    try {
+			Yaka.analyse();
+		} catch (ParseException e) {
+			fail(e.getMessage());
+		}
+	}
+	
+	/**
+	 * Compile the program to ASM code.
+	 * @param program The Yaka program.
+	 */
+	private static void compileToASM(String program) {
+		InputStream input = new ByteArrayInputStream(program.getBytes());
+		Yaka.ReInit(input);
+		Yaka.initVariables(false);
 	    try {
 			Yaka.analyse();
 		} catch (ParseException e) {
