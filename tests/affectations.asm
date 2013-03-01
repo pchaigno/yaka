c8b03fa11c65de1b8d894f753b3ec8fe8ce278eb
Paul Chaignon <midionos@laposte.net>
2013-03-01 07:53:59 +0100

    Files with programs for tests added.

---
+++ tests/affectations.asm
@@ -0,0 +1,89 @@
+; entete
+extrn lirent:proc, ecrent:proc
+extrn ecrbool:proc
+extrn ecrch:proc, ligsuiv:proc
+.model SMALL
+.586
+
+.CODE
+debut :
+STARTUPCODE
+
+; ouvrePrinc 6
+mov bp, sp
+sub sp, 6
+
+; iload -2
+push word ptr [bp-2]
+
+; iload -4
+push word ptr [bp-4]
+
+; iconst 2
+push word ptr 2
+
+; idiv
+pop bx
+pop ax
+cwd
+idiv bx
+push ax
+
+; iadd
+pop bx
+pop ax
+add ax, bx
+push ax
+
+; iconst 5
+push word ptr 5
+
+; idiv
+pop bx
+pop ax
+cwd
+idiv bx
+push ax
+
+; istore -6
+pop ax
+mov word ptr [bp-6], ax
+
+; iload -4
+push word ptr [bp-4]
+
+; iconst 3
+push word ptr 3
+
+; iload -4
+push word ptr [bp-4]
+
+; imul
+pop bx
+pop ax
+imul bx
+push ax
+
+; iadd
+pop bx
+pop ax
+add ax, bx
+push ax
+
+; iconst 4
+push word ptr 4
+
+; isub
+pop bx
+pop ax
+sub ax, bx
+push ax
+
+; istore -2
+pop ax
+mov word ptr [bp-2], ax
+
+; queue
+nop
+EXITCODE
+end debut
